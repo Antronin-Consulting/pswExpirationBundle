78f7f41eb653081f63b52c5b189ca5a5797ceda2
Péter Nagy <peter@antronin.consulting>
2026-05-09 17:20:53 +0200

    Add tests to enum translation

--- tests/Config/UnitEnumTest.php
+++ tests/Config/UnitEnumTest.php
@@ -9,36 +9,36 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UnitEnumTest extends TestCase
 {
-  public static function unitLabelProvider(): array
-  {
-    return [
-      [Unit::HOURS, 'unit.hours'],
-      [Unit::DAYS, 'unit.days'],
-      [Unit::WEEKS, 'unit.weeks'],
-      [Unit::MONTHS, 'unit.months'],
-    ];
-  }
+    public static function unitLabelProvider(): array
+    {
+        return [
+            [Unit::HOURS,  'unit.hours'],
+            [Unit::DAYS,   'unit.days'],
+            [Unit::WEEKS,  'unit.weeks'],
+            [Unit::MONTHS, 'unit.months'],
+        ];
+    }
 
-  #[DataProvider(methodName: 'unitLabelProvider')]
-  public function testLabel(Unit $unit, string $expectedLabel): void
-  {
-    self::assertSame(expected: $expectedLabel, actual: $unit->label());
-  }
+    #[DataProvider(methodName: 'unitLabelProvider')]
+    public function testLabel(Unit $unit, string $expectedLabel): void
+    {
+        self::assertSame(expected: $expectedLabel, actual: $unit->label());
+    }
 
-  public function testTrans(): void
-  {
-    $translator = $this->createMock(type: TranslatorInterface::class);
-    $translator->expects($this->once())
-      ->method(constraint: 'trans')
-      ->with(
-        'unit.days',
-        [],
-        'PswExpirationBundle',
-        'fr'
-      )
-      ->willReturn(value: 'jours');
+    public function testTrans(): void
+    {
+        $translator = $this->createMock(type: TranslatorInterface::class);
+        $translator->expects($this->once())
+            ->method(constraint: 'trans')
+            ->with(
+                'unit.days',
+                [],
+                'PswExpirationBundle',
+                'fr'
+            )
+            ->willReturn(value: 'jours');
 
-    $translated = Unit::DAYS->trans(translator: $translator, locale: 'fr');
-    self::assertSame(expected: 'jours', actual: $translated);
-  }
+        $translated = Unit::DAYS->trans(translator: $translator, locale: 'fr');
+        self::assertSame(expected: 'jours', actual: $translated);
+    }
 }
